1aaa79ab062db3e6913bf93951dada1cd872045b
Shashikr36 <shashikrpatwa36@gmail.com>
2025-04-01 23:24:20 +0530

    Update: db settings

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
-    console.log(`${error}`.bgRed);
+    console.log(`MongoDB Connection Error: ${error.message}`.bgRed);
+    process.exit(1); // Exit process with failure
   }
 };
 
